b80cb4641beebeb8104b1af308dc7d09d65bd10f
Work <andykimwork@Andys-MBP.cogeco.local>
2023-10-04 05:10:55 -0400

    Start files with some settings and readme

--- src/config/index.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-const config = {
-  googleClientID: process.env.CLIENT_ID,
-  googleClientSecret: process.env.CLIENT_SECRET,
-  riotKey: process.env.RIOT_KEY,
-};
-module.exports = config;
