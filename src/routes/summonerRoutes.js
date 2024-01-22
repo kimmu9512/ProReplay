a41cfdeccf6ef4640abc0f0c3f91254357fa7cfe
Work <andykimwork@Andys-MBP.cogeco.local>
2024-01-22 14:24:53 -0500

    updating

--- src/routes/summonerRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const summonerController = require("../controllers/summonerController");
-// unprotected routes (no token needed)
-router.post("/inGame", summonerController.checkActiveGames);
-router.get("/:summonerId/matches", summonerController.getMatches);
-router.get("/:region/:summonerName", summonerController.getSummoner);
-module.exports = router;
